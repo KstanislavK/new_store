c093a53c688866178711b0aa95dce4f7706acdd4
KstanislavK <skkolenov@gmail.com>
2021-10-26 21:16:15 +0400

    les4-1

--- authapp/forms.py
+++ authapp/forms.py
@@ -11,7 +11,7 @@ class ShopUserLoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(ShopUserLoginForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'field_control'
+            field.widget.attrs['class'] = 'form-control'
 
 
 class ShopUserRegisterForm(UserCreationForm):
